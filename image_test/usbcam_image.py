09239324cb582135788d73f3a63c340d01bbee80
Albert <albertyu@berkeley.edu>
2020-03-26 21:41:15 -0700

    Implement downsampling and resizing for USB camera into square 2**i x 2**i image.

--- image_test/usbcam_image.py
+++ image_test/usbcam_image.py
@@ -1,9 +1,15 @@
+import numpy as np
+import skimage
 import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import cv2
 sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import time
 
+RAW_IMAGE_HEIGHT = 480
+RAW_IMAGE_WIDTH = 640
+L_MARGIN = 50
+R_MARGIN = (RAW_IMAGE_WIDTH - RAW_IMAGE_HEIGHT) - L_MARGIN
 
 def stream_image(camport=0):
     cap = cv2.VideoCapture(camport)
@@ -25,12 +31,49 @@ def pull_image(camport=0, image_name=None):
         cv2.imwrite(image_name, frame)
     return frame
 
+def process_image_rgb(image, desired_h=64, desired_w=64):
+    # Currently only supporting downsampling to square, 2**i x 2**i image.
+    assert desired_h == desired_w, \
+        "desired_h: {} should equal desired_w: {}".format(desired_h, desired_w)
+    assert desired_h == int(2 ** np.round(np.log2(desired_h))), "desired_h {} not a power of 2".format(desired_h)
+    assert desired_w == int(2 ** np.round(np.log2(desired_w))), "desired_w {} not a power of 2".format(desired_w)
+
+    # flip upside-down (0), then leftside-right (1)
+    image = np.flip(image, axis=(0,1))
+    h, w, _ = image.shape
+    assert h == RAW_IMAGE_HEIGHT and w == RAW_IMAGE_WIDTH, \
+        "Dimensions {}, {} do not match expected raw image dimensions {}, {}".format(
+            h, RAW_IMAGE_HEIGHT, w, RAW_IMAGE_WIDTH
+        )
+
+    # Crop left and right.
+    image = image[:,L_MARGIN : RAW_IMAGE_WIDTH - R_MARGIN]
+
+    # Resize square image to a power of 2.
+    resize_to = next(
+        2 ** i for i in reversed(range(10))
+        if 2 ** i < image.shape[0])
+    image = skimage.transform.resize(
+        image, (resize_to, resize_to), anti_aliasing=True, mode='constant')
+
+    # Downsample 2**i x 2**i dimensioned square image.
+    height_factor = image.shape[0] // desired_h
+    width_factor = image.shape[1] // desired_w
+    image = skimage.transform.downscale_local_mean(
+        image, (width_factor, height_factor, 1))
+    image = skimage.util.img_as_ubyte(image)
+
+    return image
 
 if __name__ == "__main__":
     count = 0
     while True:
-        pull_image(0, 'randompicture_{}.png'.format(count))
+        frame = pull_image(0, 'randompicture_{}.png'.format(count))
+        frame = process_image_rgb(frame, 64, 64)
+        cv2.imwrite("randompicture_{}_processed.png".format(count), frame)
+        print("frame.shape", frame.shape)
         time.sleep(1)
         count += 1
+        break
     #stream_image()
     #time.sleep(10)
