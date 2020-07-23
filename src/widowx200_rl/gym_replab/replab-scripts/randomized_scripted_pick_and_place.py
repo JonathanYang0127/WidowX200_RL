76dbe1e00cf6f81f7a53956ec74e20ee15012898
Albert <albertyu@berkeley.edu>
2020-07-22 20:04:16 -0700

    Add pick place env/scripted policies

--- src/widowx200_rl/gym_replab/replab-scripts/randomized_scripted_pick_and_place.py
+++ src/widowx200_rl/gym_replab/replab-scripts/randomized_scripted_pick_and_place.py
@@ -10,7 +10,7 @@ from PIL import Image
 import torch
 import pickle
 
-DRAWER_OPEN_ENVS = ['Widow200Drawer-v0']
+PLACE_ENVS = ['Widow200Place-v0']
 
 def make_dirs():
     global data_save_path, video_save_path, timestamp
@@ -26,7 +26,7 @@ def make_dirs():
         os.makedirs(video_save_path)
 
 
-def scripted_drawer_open(env, data_xyz, data_joint, noise_stds, \
+def scripted_place(env, data_xyz, data_joint, noise_stds, \
         save_video, policy=None, policy_rate=0.5, image_save_dir=""):
 
     obs = env.reset()
@@ -40,11 +40,9 @@ def scripted_drawer_open(env, data_xyz, data_joint, noise_stds, \
             use_robot_state = True
 
 
-    open = False
-    lift = False
-    move_to_grasp = False
-    lower = False
+    drop_object = False
     gripper_closed = False
+    relabel_gripper = False
 
     for i in range(args.num_timesteps):
         print(i)
@@ -59,82 +57,31 @@ def scripted_drawer_open(env, data_xyz, data_joint, noise_stds, \
                 action, _ = policy(obs['image'])
 
         else:
-            print("Scripted Policy")
+            print("Scripted Policy1")
             images.append(Image.fromarray(np.uint8(obs['render'])))
             print(obs['achieved_goal'])
-            if np.linalg.norm(obs['achieved_goal'][:2] - np.array([0.265, -0.06])) > 0.015 and not open:
-                target = np.array([0.265, -0.06, 0.17])
-                wrist_target = 0 if abs(obs['joints'][4]) < (obs['joints'][4] - 2.6) else 2.6
+            if np.linalg.norm(obs['achieved_goal'][:2] - np.array([0.265, 0.06])) > 0.015 and not drop_object:
+                target = np.array([0.265, 0.06, 0.17])
+                wrist_target = 0
                 diff = target - obs['achieved_goal']
                 wrist_diff = wrist_target -  obs['joints'][4]
-                diff *= 5
-                gripper = 0.7
+                diff *= 10
+                gripper = -0.7
                 action = np.append(diff, [[wrist_diff * 3, gripper]])
-            elif np.linalg.norm(obs['achieved_goal'][2] - 0.07) > 0.015 and not open:
-                target = np.array([0.265, -0.06, 0.07])
-                wrist_target = 0 if abs(obs['joints'][4]) < (obs['joints'][4] - 2.6) else 2.6
-                diff = target - obs['achieved_goal']
-                wrist_diff = wrist_target -  obs['joints'][4]
-                diff *= 5
-                gripper = 0.7
-                action = np.append(diff, [[wrist_diff * 3, gripper]])
-            elif np.linalg.norm(obs['achieved_goal'][1] + 0.19) > 0.01 and not lift:
-                 target = np.array([0.265, -0.2, 0.07])
-                 wrist_target = 0 if abs(obs['joints'][4]) < (obs['joints'][4] - 2.6) else 2.6
-                 diff = target - obs['achieved_goal']
-                 wrist_diff = wrist_target -  obs['joints'][4]
-                 diff *= 5
-                 diff[1] *= 5
-                 gripper = 0.7
-                 action = np.append(diff, [[wrist_diff * 3, gripper]])
-                 open = True
-            elif obs['achieved_goal'][2] - 0.175 < -0.01 and not move_to_grasp:
-                 target = np.array([0.16, -0.12, 0.24])
-                 wrist_target = 0 if abs(obs['joints'][4]) < (obs['joints'][4] - 2.6) else 2.6
-                 diff = target - obs['achieved_goal']
-                 wrist_diff = wrist_target -  obs['joints'][4]
-                 diff *= 5
-                 diff[2] = 0.8
-                 gripper = 0.7
-                 action = np.append(diff, [[wrist_diff * 3, gripper]])
-                 lift = True
-            elif np.linalg.norm(obs['achieved_goal'][:2] - np.array([0.259, -0.04])) > 0.006 and not lower:
-                target = np.array([0.26, -0.04, 0.175])
-                wrist_target = 1.2
-                diff = target - obs['achieved_goal']
-                wrist_diff = wrist_target -  obs['joints'][4]
-                diff *= 5
-                gripper = 0.7
-                action = np.append(diff, [[wrist_diff * 3, gripper]])
-                move_to_grasp = True
-            elif np.linalg.norm(obs['achieved_goal'][2] - 0.062) > 0.01 and not gripper_closed:
-                target = np.array([0.258, -0.04, 0.06])
-                wrist_target = 1.2
-                diff = target - obs['achieved_goal']
-                wrist_diff = wrist_target -  obs['joints'][4]
-                diff *= 5
-                diff[2] *= 20
-                gripper = 0.7
-                action = np.append(diff, [[wrist_diff * 3, gripper]])
-                lower = True
-                print("LOWERING")
-            elif obs['joints'][5] > 0.3:
+            else:
                 diff = np.array([0, 0, 1], dtype='float64')
                 wrist_diff = 0
                 gripper_closed = True
                 gripper = -0.7
                 action = np.append(diff, [[wrist_diff * 3, gripper]])
-            else:
-                diff = np.array([0, 0, 0.1], dtype='float64')
-                wrist_diff = 0
-                gripper_closed = True
-                gripper = -0.7
-                finished_trajectory=True
+                relabel_gripper = True
+                drop_object = True
 
             action = np.append(diff, [[wrist_diff * 3, gripper]])
             action = gym_replab.utils.add_noise_custom(action, noise_stds=noise_stds)
 
         action = gym_replab.utils.clip_action(action)
+        print(action)
 
         if action[4] < -0.5:
             gripper_closed = True
@@ -151,6 +98,12 @@ def scripted_drawer_open(env, data_xyz, data_joint, noise_stds, \
             env.open_gripper()
             return None
 
+        if relabel_gripper:
+            action[4] = 0.7
+            reward = 1
+        else:
+            reward = 0
+
         data_xyz.append([obs, action, next_obs, reward, done])
         data_joint.append([obs, info['joint_command'], next_obs, reward, done])
         obs = next_obs
@@ -164,6 +117,198 @@ def scripted_drawer_open(env, data_xyz, data_joint, noise_stds, \
         images[0].save('{}/scripted_grasp.gif'.format(video_save_path),
                        format='GIF', append_images=images[1:],
                        save_all=True, duration=100, loop=0)
+
+
+def scripted_obstacle_removal(env, data_xyz, data_joint, noise_stds, detection_mode, image0, num_objects, \
+    save_video, policy=None, policy_rate=0.5, image_save_dir=""):
+
+    env.move_to_neutral()
+    time.sleep(1.0)
+
+    loop_counter = 0
+    if detection_mode == 'depth':
+        pc_data = gym_replab.utils.get_center_and_second_pc(depth_image_service)
+        while pc_data is None or pc_data[0][0] >= 0.45:
+            env.drop_at_random_location()
+            env.move_to_neutral()
+            pc_data = gym_replab.utils.get_center_and_second_pc(depth_image_service)
+            loop_counter += 1
+            if loop_counter >= 5:
+                sys.exit(0)
+        goal, object_vector = pc_data
+    elif detection_mode == 'rgb':
+        goal = gym_replab.utils.get_random_center_rgb(image0, num_objects, image_save_dir)
+        while goal is None or goal[0] >= 0.45:
+            env.drop_at_random_location()
+            env.move_to_neutral()
+            goal = gym_replab.utils.get_random_center_rgb(image0, num_objects, image_save_dir)
+            loop_counter += 1
+            if loop_counter >= 5:
+                sys.exit(0)
+
+    goal = np.append(goal, 0.055)
+    print(goal)
+    #goal[0] += np.random.uniform(low = -0.03, high = 0.03)
+    #goal[1] += np.random.uniform(low = -0.03, high = 0.03)
+
+    goal[0] += np.random.normal(0, 0.005)
+    if goal[0] > 0.3:
+        goal[0] += 0.02
+    #if goal[0] < 0.22:
+        #goal[0] -= 0.01
+    if goal[1] < -0.03:
+        goal[1] -= 0.02
+    if goal[1] < -0.17:
+        goal[1] -= 0.02
+    if goal[1] > 0.04:
+        goal[1] += 0.02
+    goal[1] += np.random.normal(0, 0.006) #0.015
+
+    goal[2] += np.random.uniform(low = -0.002, high = 0.005)
+
+
+    print("GOAL HEIGHT: ", goal[2])
+    goal = np.clip(goal, env._safety_box.low, env._safety_box.high)
+    env.set_goal(goal)
+    obs = env.reset()
+    quat = ik.get_cartesian_pose()[3:]
+
+    low_clip = env.action_space.low[:5]
+    high_clip = env.action_space.high[:5]
+
+    #termination_height = np.random.uniform(0.11, 0.13)
+    random_rotate = np.random.uniform(-1.5, 1.5)
+    print(random_rotate)
+    wrist_rotate = obs['joints'][5] + random_rotate
+    wrist_rotate = min(2.6, wrist_rotate)
+    end_wrist_target = -0.4 if abs(-0.4 - wrist_rotate) > abs(2.2 - wrist_rotate) else 2.2
+    images = []
+
+    if policy is not None:
+        try:
+            action = policy(obs['image'])
+            use_robot_state = False
+        except:
+            use_robot_state = True
+
+
+    gripper_closed = False
+    dropping = False
+    augment_gripper_action = False
+    for i in range(args.num_timesteps):
+        print(i)
+        print("State:", obs['state'])
+        images.append(Image.fromarray(np.uint8(obs['render'])))
+        if np.random.rand() < policy_rate:
+            print("Checkpoint Policy")
+            if use_robot_state:
+                action, _ = policy(np.append(obs['image'], obs['state']))
+            else:
+                action, _ = policy(obs['image'])
+
+        else:
+            print("Scripted Policy")
+            reduce_noise = False
+            if np.linalg.norm((obs['desired_goal'] - obs['achieved_goal'])[:2]) > 0.09 \
+                and not gripper_closed:
+                diff = obs['desired_goal'] - obs['achieved_goal']
+                diff[2] = 0.17 - obs['achieved_goal'][2]
+                diff *= 5
+                wrist_diff = wrist_rotate - obs['joints'][4]
+                gripper = 0.7
+                print('Moving to object')
+            elif (abs(obs['desired_goal'][2] - obs['achieved_goal'][2]) > 0.01 \
+                 or abs(obs['desired_goal'][2] - obs['achieved_goal'][2]) > 0.01) \
+                 and not gripper_closed:
+                diff = obs['desired_goal'] - obs['achieved_goal']
+                diff *= 2
+                diff[2] *= 4
+                wrist_diff = wrist_rotate - obs['joints'][4]
+                gripper = 0.7
+                print('Lowering arm')
+            elif obs['joints'][5] > 0.5:
+                diff = np.array([0, 0, 0], dtype='float64')
+                diff *= 5
+                wrist_diff = 0
+                gripper_closed = True
+                gripper = -0.7
+                print('Grasping object')
+            elif np.linalg.norm(obs['achieved_goal'] - np.array([0.23, -0.3, 0.16])) > 0.03 \
+                and not dropping:
+                target = np.array([0.23, -0.31, 0.16])
+                diff = target - obs['achieved_goal']
+                wrist_diff = end_wrist_target - obs['joints'][4]
+                diff *= 10
+                diff[2] *= 4
+                gripper = -0.7
+                action = np.append(diff, [[wrist_diff * 3, gripper]])
+                print('Moving out of tray')
+            else:
+                target = np.array([0.23, -0.35, 0.16])
+                diff = target - obs['achieved_goal']
+                wrist_diff = end_wrist_target - obs['joints'][4]
+                diff *= 5
+                gripper = -0.7
+                action = np.append(diff, [[wrist_diff * 3, gripper]])
+                augment_gripper_action = True
+                print('Dropping')
+
+            action = np.append(diff, [[wrist_diff * 3, gripper]])
+            if reduce_noise:
+                action = gym_replab.utils.add_noise_custom(action, noise_stds=np.array(noise_stds) / 3)
+            else:
+                action = gym_replab.utils.add_noise_custom(action, noise_stds=noise_stds)
+
+        print(action)
+        action = gym_replab.utils.clip_action(action)
+
+        if action[4] < -0.5:
+            gripper_closed = True
+        elif action[4] > 0.5:
+            gripper_closed = False
+
+        if obs['achieved_goal'][1] + action[1] > 0.04 and not gripper_closed:
+            action[1] = 0.04 - obs['achieved_goal'][1]
+
+        next_obs, reward, done, info = env.step(action)
+
+        if info['timeout']:
+            env.open_gripper()
+            return None
+
+        if augment_gripper_action:
+            action[4] = 0.7 + np.random.uniform(0.1)
+            reward = 1
+        else:
+            reward = 0
+
+        print("REWARD:", reward)
+        data_xyz.append([obs, action, next_obs, reward, done])
+        data_joint.append([obs, info['joint_command'], next_obs, reward, done])
+        obs = next_obs
+
+        if done:
+            env.open_gripper()
+            break
+
+    make_dirs()
+    if save_video:
+        images[0].save('{}/scripted_grasp.gif'.format(video_save_path),
+                       format='GIF', append_images=images[1:],
+                       save_all=True, duration=100, loop=0)
+    return goal
+
+
+def augment_data_continuous(data_xyz, data_joint):
+    if len(data_xyz) == 0:
+        return False
+    object_grasped = env.check_if_object_grasped()
+    if not object_grasped:
+        for i in range(len(data_xyz)):
+            data_xyz[i][3] = 0
+            data_joint[i][3] = 0
+
+    return object_grasped
 
 
 def store_trajectory(data_xyz, data_joint, params=None):
@@ -229,25 +374,30 @@ def main(args):
 
         data_xyz = []
         data_joint = []
+        goal = np.array([0, 0, 0])
 
-        if args.env in DRAWER_OPEN_ENVS:
+        if args.env in PLACE_ENVS:
             noise_stds = [args.noise_std*3]*6
             noise_stds[4] = args.noise_std
             scripted_drawer_open(env, data_xyz, data_joint, noise_stds, \
                 (i%args.video_save_frequency) == 0, policy=policy, policy_rate=args.policy_rate, image_save_dir="")
-            env.close_drawer()
+            
+       if args.image_save_dir != "" and object_grasped:
+                image0 = gym_replab.utils.cv2.imread(args.image_save_dir + '/image0.png')
 
-        store_trajectory(data_xyz, data_joint)
+        if goal is not None:
+            store_trajectory(data_xyz, data_joint, {'goal': goal})
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--env", type=str,
-                        choices=tuple(DRAWER_OPEN_ENVS),
+                        choices=tuple(DRAWER_OPEN_ENVS + OBSTABLE_REMOVAL_ENVS),
                         required=True)
     parser.add_argument("-d", "--data_save_directory", type=str, default="WidowX200GraspV5ShortTest")
     parser.add_argument("--noise_std", type=float, default=0.01)
     parser.add_argument("--detection_mode", type=str, default='depth')
+    parser.add_argument("--num_objects", type=int, default=1)
     parser.add_argument("--num_trajectories", type=int, default=50000)
     parser.add_argument("--num_timesteps", type=int, default=40)
     parser.add_argument("--video_save_frequency", type=int,
@@ -272,7 +422,7 @@ if __name__ == '__main__':
         grasp_detector='background_subtraction', transpose_image=True)._start_rospy()
     #env.set_low_firmware_gains()
     env.set_custom_firmware_gains(1.2)
-    
+
     depth_image_service = env.depth_image_service
 
     if args.checkpoint == "":
